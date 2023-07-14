061bce8127604b0a0c1df01e1e63c8c688e2864a
LeeHaEun1 <yumyoungssi@gmail.com>
2023-07-14 12:21:18 +0900

    Unit Block Fall 구현

--- Source/Tetris3D/GameLevel/UnitBlockPawn.cpp
+++ Source/Tetris3D/GameLevel/UnitBlockPawn.cpp
@@ -3,3 +3,16 @@
 
 #include "GameLevel/UnitBlockPawn.h"
 
+// 일정 시간이 지날때마다 한 칸씩 아래로 떨어지도록
+void AUnitBlockPawn::Tick(float _Delta)
+{
+	currentTime += _Delta;
+	if (currentTime > defaultFallingSpeed)
+	{
+		// block = self을 한 칸 떨어트리고
+		this->SetActorLocation(this->GetActorLocation()-FVector(0, 0, blockSizeZ));
+
+		// currentTime initialize
+		currentTime = 0;
+	}
+}
--- Source/Tetris3D/GameLevel/UnitBlockPawn.h
+++ Source/Tetris3D/GameLevel/UnitBlockPawn.h
@@ -13,5 +13,15 @@ UCLASS()
 class TETRIS3D_API AUnitBlockPawn : public ATetrisPawn
 {
 	GENERATED_BODY()
-	
+
+protected:
+	void Tick(float _Delta) override;
+
+private:
+	UPROPERTY(Category = "[ Unit Block Value ]", EditAnywhere, DisplayName = "Default Falling Speed")
+	float defaultFallingSpeed = 1.0f;
+	UPROPERTY(Category = "[ Unit Block Value ]", EditAnywhere, DisplayName = "Block Size Z")
+	float blockSizeZ = 100.0f;
+
+	float currentTime = 0.0f;
 };
