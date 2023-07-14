263b85d8e38bb5051ecd51a98b533e55260446db
LeeHaEun1 <yumyoungssi@gmail.com>
2023-07-14 11:01:24 +0900

    Map 동적 생성 Logic 완성

--- enc_temp_folder/61cf6127c1785368015fda589fc316c/TetrisGamemode.cpp
+++ enc_temp_folder/61cf6127c1785368015fda589fc316c/TetrisGamemode.cpp
@@ -1,4 +1,4 @@
-// F`ll out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "GameLevel/TetrisGamemode.h"
@@ -10,7 +10,7 @@ void ATetrisGamemode::CreateWall(FVector mapSize, FVector blockSize, TSubclassOf
 	Trans0.SetLocation({ -1 * blockSize.X, -1 * blockSize.Y, -1 * blockSize.Z });
 	AActor* Wall0 = GetWorld()->SpawnActor<AActor>(wallObject, Trans0);
 
-	// X, Y, Z Axis
+	// X, Y, Zรเ
 	for (int i = 0; i < mapSize.X; i++)
 	{
 		FTransform Trans;
@@ -28,34 +28,5 @@ void ATetrisGamemode::CreateWall(FVector mapSize, FVector blockSize, TSubclassOf
 		FTransform Trans;
 		Trans.SetLocation({ -1 * blockSize.X, -1 * blockSize.Y, i * blockSize.Z });
 		AActor* WallZ = GetWorld()->SpawnActor<AActor>(wallObject, Trans);
-	}
-
-	// XY, XZ, YZ Face
-	for (int i = 0; i < mapSize.X; i++)
-	{
-		for (int j = 0; j < mapSize.Y; j++)
-		{
-			FTransform Trans;
-			Trans.SetLocation({ i * blockSize.X, j * blockSize.Y, -1 * blockSize.Z });
-			AActor* WallXY = GetWorld()->SpawnActor<AActor>(wallObject, Trans);
-		}
-	}
-	for (int i = 0; i < mapSize.X; i++)
-	{
-		for (int j = 0; j < mapSize.Z; j++)
-		{
-			FTransform Trans;
-			Trans.SetLocation({ i * blockSize.X, -1 * blockSize.Y, j * blockSize.Z });
-			AActor* WallXZ = GetWorld()->SpawnActor<AActor>(wallObject, Trans);
-		}
-	}
-	for (int i = 0; i < mapSize.Y; i++)
-	{
-		for (int j = 0; j < mapSize.Z; j++)
-		{
-			FTransform Trans;
-			Trans.SetLocation({ -1 * blockSize.X, i * blockSize.Y, j * blockSize.Z });
-			AActor* WallYZ = GetWorld()->SpawnActor<AActor>(wallObject, Trans);
-		}
 	}
 }
